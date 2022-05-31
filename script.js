f238865d6ec13050eca6a05bd11853f78dbf6a09
James G <stackz@protonmail.com>
2022-05-31 17:54:32 -0400

    added todays date to top of app

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 var rootEl = $('root');
-
+$("#currentDay").text(moment().format("dddd, MMM Do"));
 var todaysDate = moment().format('MMM d, YYYY');
 for (var i = 9; i < 18; i++) {
-    var tr = $('<tr>').attr('id', i); 
-    var hourCell = $('<td>').text(i);
-    var eventCell = $('<td>').addClass('events-cell');
+    var colorKey = "";
+    if (i < moment().hours()) {
+        colorKey = "past"
+    } else if (i === moment().hours()) {
+        colorKey = "present"
+    } else {
+        colorKey = "future"
+    }
+    var hourDisplay = "";
+    if (i < 12) {
+        hourDisplay = i+"AM"
+    } else if (i === 12) {
+        hourDisplay = i+"PM"
+    } else {
+        hourDisplay = i-12 + "PM"
+    }
+    var tr = $('<tr>').attr('id', i);
+    var hourCell = $('<td>').text(hourDisplay);
+    var eventCell = $('<td>').addClass('events-cell ' + colorKey);
     var textArea = $('<textarea>').addClass('description').val(localStorage.getItem(i));
     var buttonCell = $('<td>')
-    var button = $('<button>').text("save").addClass('btn save-btn').on("click", function(){
+    var button = $('<button>').text("save").addClass('btn save-btn').on("click", function () {
         var activity = $(this).parent().siblings('.events-cell').children().val().trim();
         var hourKey = $(this).parent().parent().attr("id");
         localStorage.setItem(hourKey, activity);
     })
     $(".table").append(tr.append(hourCell, eventCell.append(textArea), buttonCell.append(button)));
-     
+    //$("#currentday").append.text(todaysDate);
 }
 
